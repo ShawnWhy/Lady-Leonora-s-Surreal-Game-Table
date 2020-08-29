31c8ec5d6c67acc290135de132ac658939265534
Shawn Yu <shawnyudesign@gmail.com>
2020-08-29 15:35:00 -0400

    sdsds

---
+++ leonorasgamingtable/build/precache-manifest.2d824abcb2481f69c2ea6eda3d1771d8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6974ebe65943adc7852280cb41ec2e4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e6d269eedee6ad13686",
+    "url": "/static/css/main.ff10a5a0.chunk.css"
+  },
+  {
+    "revision": "574bd432df86c0a6aaf1",
+    "url": "/static/js/2.66cc9698.chunk.js"
+  },
+  {
+    "revision": "f32f0ca7ebf041d455c7dc248f3cb124",
+    "url": "/static/js/2.66cc9698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6d269eedee6ad13686",
+    "url": "/static/js/main.5ff6d08a.chunk.js"
+  },
+  {
+    "revision": "4c7d51af44a9034005ae",
+    "url": "/static/js/runtime-main.239f0dff.js"
+  },
+  {
+    "revision": "3a1776b60e2f9fa4c6667d722ec3646d",
+    "url": "/static/media/Leonora.3a1776b6.png"
+  },
+  {
+    "revision": "cd632142097d792b01e97f72d6d2dfec",
+    "url": "/static/media/Max.cd632142.png"
+  },
+  {
+    "revision": "37225b8cbd0edf670ae23e88f833edb6",
+    "url": "/static/media/andre.37225b8c.png"
+  },
+  {
+    "revision": "c4561e9bbf0bb5d04856f86d7b4785bc",
+    "url": "/static/media/background.c4561e9b.jpg"
+  },
+  {
+    "revision": "ab35b3cb08971a9ad2763df11dbed0bf",
+    "url": "/static/media/left.ab35b3cb.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cf2ae61b883872c727dc75b72bd723b8",
+    "url": "/static/media/marcel.cf2ae61b.png"
+  },
+  {
+    "revision": "42b65208268cf48997f55c705430670f",
+    "url": "/static/media/right.42b65208.png"
+  }
+]);
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const io = require("socket.io").listen(server, {
 
 app.use(express.static("leonorasgamingtable/build"));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname,  "leonorasgamingtable/build", "index.html"));
 });
 const PORT = process.env.PORT || 3001 ;
